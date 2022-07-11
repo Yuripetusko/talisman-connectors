e2902ae0907fce588fc608f6df2485b6118e0c4a
Kheops <26880866+0xKheops@users.noreply.github.com>
2022-07-11 11:14:50 +0900

    feat: init wagmi

---
+++ apps/wagmi-v3-example/README.md
@@ -0,0 +1,3 @@
+# Wagmi v3 example
+
+> Copied from official [wagmi repository](https://github.com/tmm/wagmi), meant to ensure Talisman connector behaves as expected.
---
+++ packages/wagmi-connector/src/index.ts
@@ -0,0 +1,69 @@
+import {
+  Chain,
+  ConnectorNotFoundError,
+  InjectedConnector,
+  ResourceUnavailableError,
+  RpcError,
+  UserRejectedRequestError
+} from '@wagmi/core'
+
+export type TalismanConnectorOptions = {}
+
+export class TalismanConnector extends InjectedConnector {
+  readonly id = 'talisman'
+  readonly ready = typeof window != 'undefined' && !!(window as any).talismanEth
+
+  constructor({
+    chains,
+    options: options_
+  }: {
+    chains?: Chain[]
+    options?: TalismanConnectorOptions
+  } = {}) {
+    super({
+      chains,
+      options: {
+        name: 'Talisman',
+        shimDisconnect: true,
+        shimChainChangedDisconnect: true,
+        ...options_
+      }
+    })
+  }
+
+  async connect({ chainId }: { chainId?: number } = {}) {
+    try {
+      const provider = await this.getProvider()
+      if (!provider) throw new ConnectorNotFoundError()
+
+      if (provider.on) {
+        provider.on('accountsChanged', this.onAccountsChanged)
+        provider.on('chainChanged', this.onChainChanged)
+        provider.on('disconnect', this.onDisconnect)
+      }
+
+      this.emit('message', { type: 'connecting' })
+
+      const account = await this.getAccount()
+      // Switch to chain if provided
+      let id = await this.getChainId()
+      let unsupported = this.isChainUnsupported(id)
+      if (chainId && id !== chainId) {
+        const chain = await this.switchChain(chainId)
+        id = chain.id
+        unsupported = this.isChainUnsupported(id)
+      }
+
+      return { account, chain: { id, unsupported }, provider }
+    } catch (error) {
+      if (this.isUserRejectedRequestError(error)) throw new UserRejectedRequestError(error)
+      if ((<RpcError>error).code === -32002) throw new ResourceUnavailableError(error)
+      throw error
+    }
+  }
+
+  async getProvider() {
+    if (typeof window === 'undefined') return
+    return (window as any).talismanEth
+  }
+}
